52067a0d0d9141440fba091591ed668372273fc4
natalia <mellomartins.naty@gmail.com>
2021-08-26 17:48:20 -0300

    requisito 2

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+// Tive uma aula sobre esse projeto com o aluno Ícaro Emanuel, consegui entender a lógica do assunto através do projeto dele. Muitas das funções aqui tem como referência seu projeto.
+// link: https://github.com/tryber/sd-014-b-project-shopping-cart/pull/36
+
 function createProductImageElement(imageSource) {
-  const img = document.createElement('img');
-  img.className = 'item__image';
+  const img = document.createElement("img");
+  img.className = "item__image";
   img.src = imageSource;
   return img;
 }
@@ -12,27 +15,22 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) { // 2. definindo o padrão dos parâmetros para construir o objeto
+  const section = document.createElement("section");
+  section.className = "item";
 
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createCustomElement("span", "item__sku", sku));
+  section.appendChild(createCustomElement("span", "item__title", name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  section.appendChild(
+    createCustomElement("button", "item__add", "Adicionar ao carrinho!")
+  );
 
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
+// 4.
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
@@ -40,23 +38,44 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-// Consultei o repositório do Ícaro Emanuel para corrigir a função
-// link: https://github.com/tryber/sd-014-b-project-shopping-cart/pull/36/files
-const fetchProducts = () => {
-  const apiUrl = fetch('https://api.mercadolibre.com/sites/MLB/search?q=$computador')
-    .then((response) => response.json())
-    .then(({ results }) => results.forEach((product) => {
-      const products = {
-        sku: product.id,
-        name: product.title,
-        image: product.thumbnail,
-      };
-      const productsList = document.querySelector('.items');
-      productsList.appendChild(createProductItemElement(products));
-    }));
-    return apiUrl;
-};
+function getSkuFromProductItem(item) {
+  return item.querySelector("span.item__sku").innerText;
+}
 
-window.onload = () => { 
+function cartItemClickListener(event) {
+  // coloque seu código aqui
+}
+
+/*       REQUISITO 1 (passos 1-2)      */
+/* 1. */
+function fetchProducts() {
+  fetch('https://api.mercadolibre.com/sites/MLB/search?q=$computador')
+    .then((response) => response.json())
+    .then(({ results }) => {
+      results.forEach((product) => {
+        const productsList = document.querySelector('.items');
+        productsList.appendChild(createProductItemElement(product)); // chama a função createProductItemElement, usando como parâmetro cada produto do data, para construir o objeto, então o adiciona como filho da lista de produtos.
+      });
+    }); 
+}
+
+/*       REQUISITO 2 (passos 3-5)     */
+/* 3. */
+function addProductToCart(sku) {
+  fetch(`https://api.mercadolibre.com/items/${sku}`)
+    .then((response) => response.json())
+    .then((product) => {
+      const cartItemsList = document.querySelector('.cart__items');
+      cartItemsList.appendChild(createCartItemElement(product));
+    });
+}
+
+// 5.
+document.addEventListener('click', (event) => {
+  if (event.target.className
+    === 'item__add') addProductToCart(getSkuFromProductItem(event.target.parentElement));
+});
+
+window.onload = () => {
   fetchProducts();
 };
